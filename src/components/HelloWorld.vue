9cf58af3f25376e6b84f676b55c91d92826a862b
vb <luckytezuka@gmail.com>
2022-11-08 23:59:17 +0800

    引入axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,31 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-<div>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="about">
+    <button @click="login">login</button>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-    >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<script>
+import User from '@/api/user';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  computed: {},
+  created() {
+    this.login();
+  },
+  methods: {
+    async login() {
+      const res = await User.login(this.username, this.password);
+      console.log(res);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
 </style>
