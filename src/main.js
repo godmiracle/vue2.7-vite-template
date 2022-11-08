24da7a551af668863074cdf0ffa625862abefa83
vb <luckytezuka@gmail.com>
2022-11-08 23:46:35 +0800

    引入vue router vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import './style.css'
 import App from './App.vue'
+import store from './store';
+import router from './router';
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
